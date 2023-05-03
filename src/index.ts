d2637671f1faf92e1d0dfc96ffe9b070a2b3e587
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2023-05-03 22:29:44 +0200

    final changes

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,33 @@ import { z } from 'zod';
 import { match } from 'ts-pattern';
 import { tsFetch } from './utils.js';
 
-// const items = [1, 2, 3, 4];
-// const option = rust.createOption(() => items.find((i) => i === 3));
+const items = [1, 2, 3, 4];
+const option = rust.createOption(() => items.find((i) => i === 3));
 
-// if (option.type === 'Some') {
-//   console.log('option value:', option.value);
-// }
+if (option.type === 'Some') {
+  console.log('option value:', option.value);
+}
 
-// const result = rust.createResult(() => items.find((i) => i === 5), Error('number not found'));
+const result = rust.createResult(() => items.find((i) => i === 5), Error('number not found'));
 
-// if (result.type === 'Ok') {
-//   console.log('result value:', result.value);
-// } else {
-//   console.log('result error:', result.err.message);
-// }
+if (result.type === 'Ok') {
+  console.log('result value:', result.value);
+} else {
+  console.log('result error:', result.err.message);
+}
 
-// console.log('\n\n\nunwrap option', rust.unwrapOr(option, 10));
+console.log('\n\n\nunwrap option', rust.unwrapOr(option, 10));
 
-// console.log('unwrap result', rust.unwrapOr(result, 20));
+console.log('unwrap result', rust.unwrapOr(result, 20));
 
-// const users = await rust.createAsyncResult(
-//   () =>
-//     tsFetch(
-//       z.array(z.object({ id: z.number(), name: z.string() })),
-//       'https://jsonplaceholder.typicode.com/users'
-//     ),
-//   'users not found'
-// );
+const users = await rust.createAsyncResult(
+  () =>
+    tsFetch(
+      z.array(z.object({ id: z.number(), name: z.string() })),
+      'https://jsonplaceholder.typicode.com/users'
+    ),
+  'users not found'
+);
 
 // if (users.type === 'Ok') {
 //   console.log('\n\nusers', users.value);
@@ -44,7 +44,7 @@ import { tsFetch } from './utils.js';
 // );
 
 // with ts-pattern
-// match(users)
-//   .with({ type: 'Ok' }, ({ value }) => console.log(value))
-//   .with({ type: 'Error' }, ({ err }) => console.log(err))
-//   .exhaustive();
+match(users)
+  .with({ type: 'Ok' }, ({ value }) => console.log(value))
+  .with({ type: 'Error' }, ({ err }) => console.log(err))
+  .exhaustive();
--- src/rust.ts
+++ src/rust.ts
@@ -21,53 +21,53 @@ export const createOption = <T>(fn: () => Maybe<T>): Option<T> => {
 };
 
 // The "Result" type
-// type Ok<T> = { type: 'Ok'; value: T };
-// const Ok = <T>(value: T): Ok<T> => ({ type: 'Ok', value });
+type Ok<T> = { type: 'Ok'; value: T };
+const Ok = <T>(value: T): Ok<T> => ({ type: 'Ok', value });
 
-// type Err<E> = { type: 'Error'; err: E };
-// const Err = <E>(err: E): Err<E> => ({ type: 'Error', err });
+type Err<E> = { type: 'Error'; err: E };
+const Err = <E>(err: E): Err<E> => ({ type: 'Error', err });
 
-// type Result<T, E> = Ok<T> | Err<E>;
+type Result<T, E> = Ok<T> | Err<E>;
 
-// export const createResult = <T, E>(fn: () => Maybe<T>, error: E): Result<T, E> => {
-//   try {
-//     const value = fn();
-//     return stripNullAndUndefined(value) ? Ok(value) : Err(error);
-//   } catch (_error) {
-//     return Err(error);
-//   }
-// };
+export const createResult = <T, E>(fn: () => Maybe<T>, error: E): Result<T, E> => {
+  try {
+    const value = fn();
+    return stripNullAndUndefined(value) ? Ok(value) : Err(error);
+  } catch (_error) {
+    return Err(error);
+  }
+};
 
-// export function unwrap<T>(arg: Option<T>): T;
-// export function unwrap<T, E>(arg: Result<T, E>): T;
-// export function unwrap<T>(arg: any): T {
-//   if (arg.type === 'Some') return arg.value;
-//   if (arg.type === 'Ok') return arg.value;
-//   if (arg.type === 'Error') {
-//     throw arg.err;
-//   }
+export function unwrap<T>(arg: Option<T>): T;
+export function unwrap<T, E>(arg: Result<T, E>): T;
+export function unwrap<T>(arg: any): T {
+  if (arg.type === 'Some') return arg.value;
+  if (arg.type === 'Ok') return arg.value;
+  if (arg.type === 'Error') {
+    throw arg.err;
+  }
 
-//   throw Error('Cannot unwrap value!');
-// }
+  throw Error('Cannot unwrap value!');
+}
 
-// export function unwrapOr<T>(arg: Option<T>, defaultValue: T): T;
-// export function unwrapOr<T, E>(arg: Result<T, E>, defaultValue: T): T;
-// export function unwrapOr<T>(arg: any, defaultValue: T): T {
-//   try {
-//     return unwrap(arg);
-//   } catch (e) {
-//     return defaultValue;
-//   }
-// }
+export function unwrapOr<T>(arg: Option<T>, defaultValue: T): T;
+export function unwrapOr<T, E>(arg: Result<T, E>, defaultValue: T): T;
+export function unwrapOr<T>(arg: any, defaultValue: T): T {
+  try {
+    return unwrap(arg);
+  } catch (e) {
+    return defaultValue;
+  }
+}
 
-// export const createAsyncResult = async <T, E>(
-//   fn: () => Promise<Maybe<T>>,
-//   error: E
-// ): Promise<Result<T, E>> => {
-//   try {
-//     const val = await fn();
-//     return createResult(() => val, error);
-//   } catch (_error) {
-//     return Err(error);
-//   }
-// };
+export const createAsyncResult = async <T, E>(
+  fn: () => Promise<Maybe<T>>,
+  error: E
+): Promise<Result<T, E>> => {
+  try {
+    const val = await fn();
+    return createResult(() => val, error);
+  } catch (_error) {
+    return Err(error);
+  }
+};
